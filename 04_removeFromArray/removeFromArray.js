a194d11ccdaf20bbc420ff09b5c6d39016f488f3
Brook <37387041+cannon1062@users.noreply.github.com>
2024-01-16 20:19:08 -0500

    Complete exercises 1 through 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...rm) {
+    let i = 0;
+    while (i < array.length) {
+        let j = 0;
+        while (j < rm.length) {
+            if (array[i] === rm[j]) {
+                array.splice(i, 1);
+                i--;
+            }
+            j++;
+        }
+        i++;
+    }
+    return array;
 };
 
 // Do not edit below this line
