0382e46cc1bce6107b007dacfe2143fd3c60d4e6
Brook <37387041+cannon1062@users.noreply.github.com>
2024-01-16 21:50:58 -0500

    Complete exercises 5 through 7

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (a < 0 || 
+        b < 0 || 
+        typeof a !== 'number' || 
+        typeof b !== 'number') {
+        return "ERROR";
+    }
+    let first, last;
+    if (a < b) {
+        first = a;
+        last = b;
+    } else {
+        first = b;
+        last = a;
+    }
+    let sum = 0;
+    for (let i = first; i <= last; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
