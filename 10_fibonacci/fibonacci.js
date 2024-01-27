f8b0cab4864780d7a3456196b06916f40a2337ea
Brook <37387041+cannon1062@users.noreply.github.com>
2024-01-27 10:30:30 -0500

    Complete 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (+num === 0) {
+        return 0;
+    }
+    if (+num < 0) {
+        return "OOPS";
+    }
+    let fOlder = 0;
+    let fOld = 1;
+    let fN = fOlder + fOld; // initializes at fTwo
+    let i = 2; // Loop starts at fTwo
+    while (i < +num) {
+        fOlder = fOld;
+        fOld = fN;
+        fN = fOld + fOlder;
+        i++;
+    }
+    return fN;
 };
 
 // Do not edit below this line
