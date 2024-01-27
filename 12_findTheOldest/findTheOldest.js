48e7a75f13b236cea578898e691dc6dba1b59ede
Brook <37387041+cannon1062@users.noreply.github.com>
2024-01-27 16:38:16 -0500

    Complete 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,9 @@
-const findTheOldest = function() {
-
-};
-
+const findTheOldest = function(people) {
+    let currentYear = new Date().getFullYear();
+    return people.sort((a, b) => {
+        return ((Object.hasOwn(b, 'yearOfDeath') ? b.yearOfDeath : currentYear) - b.yearOfBirth) - 
+               ((Object.hasOwn(a, 'yearOfDeath') ? a.yearOfDeath : currentYear) - a.yearOfBirth);
+    })[0];
+;}
 // Do not edit below this line
 module.exports = findTheOldest;
