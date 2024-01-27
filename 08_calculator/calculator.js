11da96220e3f0c43960a3ef5d674a3ae3c0afbbe
Brook <37387041+cannon1062@users.noreply.github.com>
2024-01-26 21:36:47 -0500

    Complete 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+	return a.reduce((a, b) => a + b, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+  return a.reduce((a, b) => a * b, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if (a === 0) {
+    return 1;
+  }
+  let i = a;
+  while (i > 1) {
+    i--;
+    a *= i;
+  }
+  return a;
 };
 
 // Do not edit below this line
