1a9cf44d24450da6ee73f3970c9714be8aa7bb68
Brook <37387041+cannon1062@users.noreply.github.com>
2024-01-26 22:07:14 -0500

    Complete 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+     newStr = str.match(/[0-z]/ig).join('');
+    return newStr.toLowerCase() === newStr.split('').reverse().join('').toLowerCase();
 };
 
 // Do not edit below this line
