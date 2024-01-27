93c48886d4469ce152452764c4daaa72e0217753
Brook <37387041+cannon1062@users.noreply.github.com>
2024-01-27 10:48:33 -0500

    Add additional test to 11_getTheTitles

--- 11_getTheTitles/getTheTitles.spec.js
+++ 11_getTheTitles/getTheTitles.spec.js
@@ -10,9 +10,27 @@ describe('getTheTitles', () => {
         title: 'Book2',
         author: 'Name2'
       }
-    ]
+    ];
 
+    const books2 = [
+      {
+        title: 'The Sorcerer\'s Stone',
+        author: 'J.K. Rowling'
+      },
+      {
+        title: 'The Chamber of Secrets',
+        author: 'J.K. Rowling'
+      },
+      {
+        title: 'The Prisoner of Azkaban',
+        author: 'J.K. Rowling'
+      }
+    ]
   test('gets titles', () => {
-    expect(getTheTitles(books)).toEqual(['Book','Book2']);
-  });
+    expect(getTheTitles(books)).toEqual(['Book', 'Book2']);  
+  }),
+  test('gets titles', () => {
+    expect(getTheTitles(books2)).toEqual(['The Sorcerer\'s Stone',
+      'The Chamber of Secrets', 'The Prisoner of Azkaban']);
+  })
 });
